c376f34b32580bfb04a56905e09e71548b7e0b05
Nikolay Grunin <groonyn@gmail.com>
2018-04-03 11:47:22 +0300

    Calculator simple test

---
+++ src/test/java/com/cucumber/bdd/runner/RunCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.cucumber.bdd.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = { "pretty", "html:target/cucumber" },
+        glue = "com.cucumber.bdd.steps",
+        features = "classpath:cucumber/calculator.feature"
+)
+public class RunCalculatorTest {
+}
